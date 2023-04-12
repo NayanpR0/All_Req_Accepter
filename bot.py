4cc15ff5fc70b0543240622767e803042fa32ad8
Muhammedshifin <muhammedshifink4@gmail.com>
2023-04-12 17:46:56 +0530

    Create bot.py

---
+++ bot.py
@@ -0,0 +1,12 @@
+from pyrogram import Client
+
+Client = Client(name="auto-delete",
+             api_id=API_ID,
+             api_hash=API_HASH,
+             bot_token=BOT_TOKEN,
+             plugins={"root": "plugins"},
+             workers=300
+             )
+
+Client.start()
+print("Bot Started!")
