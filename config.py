c7f3979dce4490a67234e9e9ad43eb706571527a
Muhammedshifin <muhammedshifink4@gmail.com>
2023-04-12 18:18:29 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,3 +1,6 @@
 from os import environ
 import os
 
+API_ID = int(environ.get('API_ID', "18302370"))
+API_HASH = environ.get('API_HASH', "03c2cced4dea9b1e96dce87558dd2381")
+BOT_TOKEN = environ.get('BOT_TOKEN', "")
