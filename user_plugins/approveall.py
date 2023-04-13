14bfcc23295b9da8c5026d7a99bc0ce0557fd4db
Muhammedshifin <muhammedshifink4@gmail.com>
2023-04-13 16:33:11 +0530

    Update approveall.py

--- user_plugins/approveall.py
+++ user_plugins/approveall.py
@@ -1,3 +1,32 @@
 import asyncio
 from pyrogram import Client, filters
 from pyrogram.types import Message
+
+
+@Client.on_message(filters.command(["run", "approve"], CMND) & main_chat)                     
+async def approve(client: Client, message: Message):
+    chat=message.chat
+    await message.delete()
+    msg = await message.reply_text("Proccesing")
+    try:
+        await client.approve_all_chat_join_requests(chat.id)#1
+        await asyncio.sleep(4)
+        await client.approve_all_chat_join_requests(chat.id)#2
+        await asyncio.sleep(4)
+        await client.approve_all_chat_join_requests(chat.id)#3
+        await asyncio.sleep(4)
+        await client.approve_all_chat_join_requests(chat.id)#4
+        await asyncio.sleep(4)
+        await client.approve_all_chat_join_requests(chat.id)#5
+        await asyncio.sleep(4)
+        await client.approve_all_chat_join_requests(chat.id)#6
+        await asyncio.sleep(4)
+        await client.approve_all_chat_join_requests(chat.id)#7
+        await asyncio.sleep(4)
+        await client.approve_all_chat_join_requests(chat.id)#8
+        await asyncio.sleep(4)
+        await client.approve_all_chat_join_requests(chat.id)#9
+        await asyncio.sleep(4)
+        await client.approve_all_chat_join_requests(chat.id)#10
+        await msg.edit("Complted approved all Chat Join Reqs")
+    except FloodWait as x:
