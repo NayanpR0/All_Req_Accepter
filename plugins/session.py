38a5ec3bb0c6df5810a8dedaedd8e2a7e3829289
Muhammedshifin <muhammedshifink4@gmail.com>
2023-04-13 11:22:56 +0530

    Update session.py

--- plugins/session.py
+++ plugins/session.py
@@ -1,1 +1,12 @@
-from pyrogram import Client
+from pyrogram import Client,  filters, enums
+from config import API_ID, API_HASH
+
+@Client.on_message(filters.command("clone") & filters.user(ADMINS))
+async def add_new_session(bot, message):
+    user = message.from_user.id
+    msg = await message.reply_text("processing")
+    try:
+        session = message.text.split(" ", maxsplit=1)[1]
+    except Exception:
+        await msg.edit("Give session")
+    
