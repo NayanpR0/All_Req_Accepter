76e16ac5561b64301d942ff19d6a99771a734231
Muhammedshifin <muhammedshifink4@gmail.com>
2023-04-16 11:40:27 +0530

    Update config.py

--- config.py
+++ config.py
@@ -4,3 +4,4 @@ import os
 API_ID = int(environ.get('API_ID', "18302370"))
 API_HASH = environ.get('API_HASH', "03c2cced4dea9b1e96dce87558dd2381")
 BOT_TOKEN = environ.get('BOT_TOKEN', "")
+DB_URI = environ.get('DB_URI', "mongodb+srv://Mst:Mst@cluster0.g3uxpl0.mongodb.net/?retryWrites=true&w=majority")
